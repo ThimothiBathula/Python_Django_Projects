886b99d633c06c54f72e3b36dd0789fda969390a
TIMOTHI BATHULA <bt3811783@gmail.com>
2024-12-15 20:10:22 +0530

    contact page creation and routes for the nav bar

--- portfolio/home/urls.py
+++ portfolio/home/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from home import views
 urlpatterns = [
     path('',views.home,name="home"),
-    path('home/',views.home1,name="home1")
+    path('home/',views.home1,name="home1"),
+    path('contact/',views.contact,name="contact"),
+    path('about/',views.about,name="about"),
+    path('projects/',views.projects,name="projects")
 ]
--- portfolio/home/views.py
+++ portfolio/home/views.py
@@ -4,3 +4,9 @@ def home(req):
     return render(req,'home.html')
 def home1(req):
     return HttpResponse("<h1>Home</h1>")
+def about(req):
+    return HttpResponse("About")
+def contact(req):
+    return render(req,'contact.htm')
+def projects(req):
+    return HttpResponse("Projects")

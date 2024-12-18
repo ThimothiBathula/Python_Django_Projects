5efa899ca6357d84322bd65154eff04823347f1b
TIMOTHI BATHULA <bt3811783@gmail.com>
2024-12-18 23:21:22 +0530

    models creation for contact form

--- portfolio/home/views.py
+++ portfolio/home/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render,HttpResponse
+from home import models
 
 def home(req):
     return render(req,'home.html')
@@ -7,6 +8,14 @@ def home1(req):
 def about(req):
     return render(req,'about.htm')
 def contact(req):
+    if req.method=='POST':
+        name=req.POST['name']
+        email=req.POST['email']
+        content=req.POST['content']
+        print(name,email,content)
+        instance=models.Contact(name=name,email=email,content=content)
+        instance.save()
+        print("saved")
     return render(req,'contact.htm')
 def projects(req):
     return render(req,"projects.htm")
